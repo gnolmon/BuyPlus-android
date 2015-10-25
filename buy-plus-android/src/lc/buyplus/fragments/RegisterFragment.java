ac019ef82614d569a1f7639fe7428964662ca644
gitkey <longcqse03034@fpt.edu.vn>
2015-10-25 20:50:25 +0700

    create Register fragment and Register laytout

--- buy-plus-android/src/lc/buyplus/fragments/RegisterFragment.java
+++ buy-plus-android/src/lc/buyplus/fragments/RegisterFragment.java
@@ -1,5 +1,137 @@
 package lc.buyplus.fragments;
 
-public class RegisterFragment {
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import lc.buyplus.R;
+import lc.buyplus.cores.CoreActivity;
+import lc.buyplus.cores.CoreFragment;
+import lc.buyplus.cores.HandleRequest;
+import lc.buyplus.customizes.Button;
+import lc.buyplus.customizes.MyEditText;
+import lc.buyplus.models.UserAccount;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterFragment extends CoreFragment implements OnClickListener {
+
+	private Button btnRegister;
+	private MyEditText edName, edPass, edCfPass, edEmail;
+
+	private CallbackManager callbackManager;
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		callbackManager = CallbackManager.Factory.create();
+		View view = inflater.inflate(R.layout.fragment_register, container, false);
+		initViews(view);
+		initModels();
+		initAnimations();
+		btnRegister = (Button) view.findViewById(R.id.btnRegister);
+
+		edName = (MyEditText) view.findViewById(R.id.edNameRegister);
+		edPass = (MyEditText) view.findViewById(R.id.edPassRegister);
+		edCfPass = (MyEditText) view.findViewById(R.id.edcfPass);
+		edEmail = (MyEditText) view.findViewById(R.id.edEmail);
+
+		btnRegister.setOnClickListener(this);
+
+		return view;
+	}
+
+	@Override
+	protected void initModels() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initViews(View view) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initAnimations() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnRegister:
+			String username = edName.getText().toString();
+			String password = edPass.getText().toString();
+			String cpassword = edCfPass.getText().toString();
+			String email = edEmail.getText().toString();
+			if (username.isEmpty()) {
+				Toast.makeText(this.getActivity(), "Please input username", 2000).show();
+			} else if (email.isEmpty()) {
+				Toast.makeText(this.getActivity(), "Please input email", 2000).show();
+			} else if (password.isEmpty()) {
+				Toast.makeText(this.getActivity(), "Please input password", 2000).show();
+			} else if (cpassword.isEmpty()) {
+				Toast.makeText(this.getActivity(), "Please confirm password", 2000).show();
+			} else {
+				api_user_register(username, cpassword);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void api_user_register(String login_name, String password){
+	 	
+    	Map<String, String> params = new HashMap<String, String>();
+		params.put("login_name", login_name);
+		params.put("password", password);
+			RequestQueue requestQueue = Volley.newRequestQueue(this.getActivity());
+			HandleRequest jsObjRequest = new HandleRequest(
+					HandleRequest.USER_REGISTER, params, 
+					new Response.Listener<JSONObject>() {
+					@Override
+					public void onResponse(JSONObject response) {
+						Log.d("api_user_register",response.toString());
+						// code here
+					}
+					}, 
+					new Response.ErrorListener() {
+						@Override
+						public void onErrorResponse(VolleyError error) {
+						}
+					});
+			requestQueue.add(jsObjRequest);
+	}
 
 }
