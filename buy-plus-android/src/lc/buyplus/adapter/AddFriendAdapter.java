b6ca67f8b78b276db869e6a45a86e7fa4cc1a188
gitkey <longcqse03034@fpt.edu.vn>
2015-11-18 02:12:24 +0700

    fix add friend layout, progressbar

--- buy-plus-android/src/lc/buyplus/adapter/AddFriendAdapter.java
+++ buy-plus-android/src/lc/buyplus/adapter/AddFriendAdapter.java
@@ -83,7 +83,7 @@ public class AddFriendAdapter extends BaseAdapter {
 			public void onClick(View v) {
 				
 				api_send_request_join_shop_to_friend(ShopInfoActivity.current_shop_id,item.getId());
-				addFriend.setText("Dang cho");
+				addFriend.setText("Đang chờ");
 				addFriend.setEnabled(false);
 				friendList.remove(pos);
 			}
