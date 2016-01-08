2c3dea76726944273191228e8ef126e2c6eb6648
gnolmon <gnolmon@gmail.com>
2016-01-08 23:47:53 +0700

    smooth announcement

--- buy-plus-android/src/lc/buyplus/adapter/AnnounmentAdapter.java
+++ buy-plus-android/src/lc/buyplus/adapter/AnnounmentAdapter.java
@@ -18,6 +18,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -42,6 +43,7 @@ import lc.buyplus.cores.FeedImageView;
 import lc.buyplus.cores.HandleRequest;
 import lc.buyplus.customizes.DialogMessage;
 import lc.buyplus.customizes.RoundedImageView;
+import lc.buyplus.customizes.RoundedViewImage;
 import lc.buyplus.fragments.CanvasFragment;
 import lc.buyplus.models.Announcement;
 import lc.buyplus.models.Shop;
@@ -56,14 +58,26 @@ public class AnnounmentAdapter extends BaseAdapter {
 	ImageLoader imageLoader = MonApplication.getInstance().getImageLoader();
 	FragmentManager mFragmentManager;
 	private OnLoadMoreListener onLoadMoreListener;
-	
-	public AnnounmentAdapter(ArrayList<Announcement> announcementList, LayoutInflater inflaterActivity, CoreActivity activity, FragmentManager mFragmentManager) {
+
+	public AnnounmentAdapter(ArrayList<Announcement> announcementList, LayoutInflater inflaterActivity,
+			CoreActivity activity, FragmentManager mFragmentManager) {
 		this.inflaterActivity = inflaterActivity;
 		this.announcementList = announcementList;
 		this.activity = activity;
 		this.mFragmentManager = mFragmentManager;
 	}
-	
+
+	static class ViewHolder {
+		public TextView name;
+		public TextView timestamp;
+		public ImageView imSaleOff;
+		public TextView tvTimeSale;
+
+		public TextView tvStatus;
+		public RoundedViewImage avaStore;
+		public ImageView feedImageView;
+	}
+
 	public OnLoadMoreListener getOnLoadMoreListener() {
 		return onLoadMoreListener;
 	}
@@ -89,90 +103,93 @@ public class AnnounmentAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder viewHolder;
 		if (inflater == null)
 			inflater = inflaterActivity;
-		if (convertView == null)
+		if (convertView == null) {
 			convertView = inflater.inflate(R.layout.item_announment, null);
+			viewHolder = new ViewHolder();
+
+			viewHolder.name = (TextView) convertView.findViewById(R.id.tvNameStore);
+			viewHolder.timestamp = (TextView) convertView.findViewById(R.id.tvTimestamp);
+			viewHolder.imSaleOff = (ImageView) convertView.findViewById(R.id.imSaleOff);
+			viewHolder.tvTimeSale = (TextView) convertView.findViewById(R.id.tvTimeSale);
+
+			viewHolder.tvStatus = (TextView) convertView.findViewById(R.id.tvStatus);
+			viewHolder.avaStore = (RoundedViewImage) convertView.findViewById(R.id.avaStore);
+			viewHolder.feedImageView = (ImageView) convertView.findViewById(R.id.imFeed);
+			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
 
 		if (imageLoader == null)
 			imageLoader = MonApplication.getInstance().getImageLoader();
 
-		TextView name = (TextView) convertView.findViewById(R.id.tvNameStore);
-		TextView timestamp = (TextView) convertView.findViewById(R.id.tvTimestamp);
-		ImageView imSaleOff = (ImageView) convertView.findViewById(R.id.imSaleOff);
-		TextView tvTimeSale = (TextView) convertView.findViewById(R.id.tvTimeSale);
-		
-		TextView tvStatus = (TextView) convertView.findViewById(R.id.tvStatus);
-		RoundedImageView avaStore = (RoundedImageView) convertView.findViewById(R.id.avaStore);
-		FeedImageView feedImageView = (FeedImageView) convertView.findViewById(R.id.imFeed);
-
 		final Announcement item = announcementList.get(position);
 
-		name.setText(item.getShop().getName());
-		
+		viewHolder.name.setText(item.getShop().getName());
+
 		long unixSeconds = item.getStart_time();
-		Date date_start = new Date(unixSeconds*1000L);
+		Date date_start = new Date(unixSeconds * 1000L);
 		unixSeconds = item.getEnd_time();
-		Date date_end = new Date(unixSeconds*1000L);
+		Date date_end = new Date(unixSeconds * 1000L);
 		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT-7"));
-		String formattedDate = sdf.format(date_start) +" đến "+ sdf.format(date_end);
-		tvTimeSale.setText(formattedDate);
-		
+		String formattedDate = sdf.format(date_start) + " đến " + sdf.format(date_end);
+		viewHolder.tvTimeSale.setText(formattedDate);
+
 		CharSequence timeAgo = DateUtils.getRelativeTimeSpanString(Long.parseLong(item.getCreated_time()),
 				System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
-		timestamp.setText(timeAgo);
-		tvStatus.setText(item.getContent());
+		viewHolder.timestamp.setText(timeAgo);
+		viewHolder.tvStatus.setText(item.getContent());
 
-		
-		avaStore.setImageUrl(item.getShop().getImage_thumbnail(), imageLoader);
-		
-		name.setOnClickListener(new OnClickListener() {
-			
+		// viewHolder.avaStore.setImageUrl(item.getShop().getImage_thumbnail(),
+		// imageLoader);
+
+		Glide.with(CanvasFragment.mActivity).load(item.getShop().getImage_thumbnail()).centerCrop()
+				.placeholder(R.drawable.loading_icon).crossFade().into(viewHolder.avaStore);
+		viewHolder.name.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				Store.current_shop_id = (item.getShop_id());
-	    	  	 api_get_shop_info(Store.current_shop_id);
-				
+				api_get_shop_info(Store.current_shop_id);
+
 			}
 		});
-		avaStore.setOnClickListener(new OnClickListener() {
-			
+		viewHolder.avaStore.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				Store.current_shop_id = (item.getShop_id());
-	    	  	 api_get_shop_info(Store.current_shop_id);
-				
+				api_get_shop_info(Store.current_shop_id);
+
 			}
 		});
-		
-		if (item.getType()==2){
-			imSaleOff.setVisibility(View.VISIBLE);
-			tvTimeSale.setVisibility(View.VISIBLE);
-		}else {
-			imSaleOff.setVisibility(View.GONE);
-			tvTimeSale.setVisibility(View.GONE);
+
+		if (item.getType() == 2) {
+			viewHolder.imSaleOff.setVisibility(View.VISIBLE);
+			viewHolder.tvTimeSale.setVisibility(View.VISIBLE);
+		} else {
+			viewHolder.imSaleOff.setVisibility(View.GONE);
+			viewHolder.tvTimeSale.setVisibility(View.GONE);
 		}
 		// Feed image
 		if (item.getPhotos().size() > 0) {
-			feedImageView.setImageUrl(item.getPhotos().get(0).getImage(), imageLoader);
-			feedImageView.setVisibility(View.VISIBLE);
-			feedImageView.setResponseObserver(new FeedImageView.ResponseObserver() {
-				@Override
-				public void onError() {
-				}
-
-				@Override
-				public void onSuccess() {
-				}
-			});
+			// viewHolder.feedImageView.setImageUrl(item.getPhotos().get(0).getImage(),
+			// imageLoader);
+			Glide.with(CanvasFragment.mActivity).load(item.getPhotos().get(0).getImage()).centerCrop()
+					.placeholder(R.drawable.loading_icon).crossFade().into(viewHolder.feedImageView);
+			viewHolder.feedImageView.setVisibility(View.VISIBLE);
+			
 		} else {
-			feedImageView.setVisibility(View.GONE);
+			viewHolder.feedImageView.setVisibility(View.GONE);
 		}
 
 		return convertView;
 	}
-	
+
 	public void api_get_shop_info(int shop_id) {
 
 		Map<String, String> params = new HashMap<String, String>();
@@ -186,28 +203,29 @@ public class AnnounmentAdapter extends BaseAdapter {
 					public void onResponse(JSONObject response) {
 						Log.d("api_get_shop_info", response.toString());
 						try {
-							if (Integer.parseInt(response.getString("error"))==2){
-								DialogMessage dialog = new DialogMessage(activity,activity.getResources().getString(R.string.end_session));
+							if (Integer.parseInt(response.getString("error")) == 2) {
+								DialogMessage dialog = new DialogMessage(activity,
+										activity.getResources().getString(R.string.end_session));
 								dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 								dialog.show();
-								SharedPreferences pre=activity.getSharedPreferences("buy_pus", 0);
-								SharedPreferences.Editor editor=pre.edit();
-								//editor.clear();
+								SharedPreferences pre = activity.getSharedPreferences("buy_pus", 0);
+								SharedPreferences.Editor editor = pre.edit();
+								// editor.clear();
 								editor.putBoolean("immediate_login", false);
 								editor.commit();
-								Intent loginActivity = new Intent(activity,LoginActivity.class);
+								Intent loginActivity = new Intent(activity, LoginActivity.class);
 								activity.startActivity(loginActivity);
-							    activity.finish();
+								activity.finish();
 
 							}
-							if (Integer.parseInt(response.getString("error"))==1){
-								DialogMessage dialog = new DialogMessage(activity,response.getString("message"));
+							if (Integer.parseInt(response.getString("error")) == 1) {
+								DialogMessage dialog = new DialogMessage(activity, response.getString("message"));
 								dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 								dialog.show();
-							}else{
+							} else {
 								Store.current_shop = new Shop(response.getJSONObject("data"));
-								Store.current_shop_name =  Store.current_shop.getName();
-								Intent shopInfoActivity = new Intent(activity,ShopInfoActivity.class);			            
+								Store.current_shop_name = Store.current_shop.getName();
+								Intent shopInfoActivity = new Intent(activity, ShopInfoActivity.class);
 								activity.startActivity(shopInfoActivity);
 							}
 						} catch (JSONException e) {
@@ -217,7 +235,8 @@ public class AnnounmentAdapter extends BaseAdapter {
 				}, new Response.ErrorListener() {
 					@Override
 					public void onErrorResponse(VolleyError error) {
-						DialogMessage dialog = new DialogMessage(activity,activity.getResources().getString(R.string.connect_problem));
+						DialogMessage dialog = new DialogMessage(activity,
+								activity.getResources().getString(R.string.connect_problem));
 						dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 						dialog.show();
 					}
